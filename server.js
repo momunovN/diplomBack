4e08c9e860230fab5f5e34eb56bbbbab8b73ca6a
Your Name <your-email@example.com>
2026-01-02 13:12:21 +0300

    CORS

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
      'https://film-live.vercel.app'
     ],
   credentials: true
